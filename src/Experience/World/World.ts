4bc2b695fde2a8d44c359dfa5951a4175a777575
alphardex <2582347430@qq.com>
2023-10-27 21:58:19 +0800

    refactor code with nijika

--- src/Experience/World/World.ts
+++ src/Experience/World/World.ts
@@ -13,7 +13,6 @@ export default class World extends kokomi.Component {
   testObject: TestObject | null;
   slider!: Slider | null;
   currentActiveMesh!: THREE.Mesh | null;
-  isAnimatingMenu!: boolean;
   isAboutOpen!: boolean;
   constructor(base: Experience) {
     super(base);
@@ -27,159 +26,11 @@ export default class World extends kokomi.Component {
       document.querySelector(".loader-screen")?.classList.add("hollow");
 
       this.currentActiveMesh = null;
-      this.isAnimatingMenu = false;
 
       this.handleSliderDetail();
       this.handleAbout();
-      this.createBlinkMenu();
       this.handleEffectSwitch();
     });
-  }
-  showDetail() {
-    document.querySelector(".detail")?.classList.remove("hollow");
-    this.slideInText(".detail");
-  }
-  hideDetail() {
-    document.querySelector(".detail")?.classList.add("hollow");
-    this.slideOutText(".detail");
-  }
-  updateDetail(id = 0) {
-    const currentDetail = config.details[id];
-    document.querySelector(".detail-title")!.textContent = currentDetail.name;
-    (document.querySelector(".detail-link")! as HTMLLinkElement).href =
-      currentDetail.link;
-    document.querySelector(".detail-text")!.innerHTML = currentDetail.desc
-      .map(
-        (item) =>
-          `<div>${item
-            .map(
-              (item2) =>
-                `<div class="slide-in-line"><div class="slide-in-line-child">${item2}</div></div>`
-            )
-            .join("")}</div>`
-      )
-      .join("");
-  }
-  showInteractHint() {
-    document.querySelector(".interact-hint")?.classList.remove("hollow");
-  }
-  hideInteractHint() {
-    document.querySelector(".interact-hint")?.classList.add("hollow");
-  }
-  showReturnHint() {
-    document.querySelector(".return-hint")?.classList.remove("hollow");
-  }
-  hideReturnHint() {
-    document.querySelector(".return-hint")?.classList.add("hollow");
-  }
-  showEffectSwitch() {
-    document.querySelector(".effect-switch")?.classList.remove("hollow");
-  }
-  hideEffectSwitch() {
-    document.querySelector(".effect-switch")?.classList.add("hollow");
-  }
-  handleAbout() {
-    document.querySelector(".about-link")?.addEventListener("click", () => {
-      this.openMenu();
-    });
-
-    document
-      .querySelector(".about-return-link")
-      ?.addEventListener("click", () => {
-        this.closeMenu();
-      });
-  }
-  disableSlider() {
-    this.slider?.ws.disable();
-    this.slider?.dd.disable();
-  }
-  enableSlider() {
-    this.slider?.ws.enable();
-    this.slider?.dd.enable();
-  }
-  get isAtDetailPage() {
-    return !!this.currentActiveMesh;
-  }
-  openMenu() {
-    if (this.isAnimatingMenu) {
-      return;
-    }
-
-    if (!this.isAtDetailPage) {
-      this.disableSlider();
-    }
-
-    const overlay = document.querySelector(".overlay") as HTMLElement;
-    const overlayPath = document.querySelector(".overlay__path");
-
-    const t1 = gsap.timeline({
-      onStart: () => {
-        overlay.style.pointerEvents = "auto";
-      },
-      onComplete: () => {
-        this.isAnimatingMenu = false;
-      },
-    });
-
-    this.isAnimatingMenu = true;
-    t1.set(overlayPath, {
-      attr: { d: "M 0 100 V 100 Q 50 100 100 100 V 100 z" },
-    })
-      .to(
-        overlayPath,
-        {
-          duration: 0.8,
-          ease: "power4.in",
-          attr: { d: "M 0 100 V 50 Q 50 0 100 50 V 100 z" },
-        },
-        0
-      )
-      .to(overlayPath, {
-        duration: 0.3,
-        ease: "power2",
-        attr: { d: "M 0 100 V 0 Q 50 0 100 0 V 100 z" },
-        onComplete: () => {
-          document.querySelector(".about-page")?.classList.remove("hollow");
-          this.slideInText(".about-page");
-        },
-      });
-  }
-  closeMenu() {
-    if (this.isAnimatingMenu) {
-      return;
-    }
-
-    if (!this.isAtDetailPage) {
-      this.enableSlider();
-    }
-
-    const overlay = document.querySelector(".overlay") as HTMLElement;
-    const overlayPath = document.querySelector(".overlay__path");
-
-    const t1 = gsap.timeline({
-      onStart: () => {
-        overlay.style.pointerEvents = "none";
-        document.querySelector(".about-page")?.classList.add("hollow");
-      },
-      onComplete: () => {
-        this.isAnimatingMenu = false;
-      },
-    });
-
-    this.isAnimatingMenu = true;
-    t1.set(overlayPath, {
-      attr: { d: "M 0 0 V 100 Q 50 100 100 100 V 0 z" },
-    })
-      .to(overlayPath, {
-        duration: 0.3,
-        ease: "power2.in",
-        attr: { d: "M 0 0 V 50 Q 50 0 100 50 V 0 z" },
-      })
-      .to(overlayPath, {
-        duration: 0.8,
-        ease: "power4",
-        attr: { d: "M 0 0 V 0 Q 50 0 100 0 V 0 z" },
-      });
   }
   handleSliderDetail() {
     this.slider?.ig.iterate((maku) => {
@@ -274,35 +125,102 @@ export default class World extends kokomi.Component {
       }
     });
   }
-  createBlinkMenu() {
-    let blinkTextMenuLinks = document.querySelectorAll(
-      ".blink-text-menu .blink-text-menu-item a"
-    );
-    blinkTextMenuLinks.forEach((link) => {
-      let letters = link.textContent?.split("");
-      link.textContent = "";
-      letters?.forEach((letter, i) => {
-        i += 1;
-        let span = document.createElement("span");
-        let delay = i / 20;
-        if (i % 2 === 0) {
-          delay -= 0.1;
-        } else {
-          delay += 0.05;
-        }
-        let letterOut = document.createElement("span");
-        letterOut.textContent = letter;
-        letterOut.style.transitionDelay = `${delay}s`;
-        letterOut.classList.add("out");
-        span.append(letterOut);
-        let letterIn = document.createElement("span");
-        letterIn.textContent = letter;
-        letterIn.style.transitionDelay = `${delay}s`;
-        letterIn.classList.add("in");
-        span.append(letterIn);
-        link.append(span);
-      });
+  disableSlider() {
+    this.slider?.ws.disable();
+    this.slider?.dd.disable();
+  }
+  enableSlider() {
+    this.slider?.ws.enable();
+    this.slider?.dd.enable();
+  }
+  showDetail() {
+    document.querySelector(".detail")?.classList.remove("hollow");
+    this.slideInText(".detail");
+  }
+  hideDetail() {
+    document.querySelector(".detail")?.classList.add("hollow");
+    this.slideOutText(".detail");
+  }
+  updateDetail(id = 0) {
+    const currentDetail = config.details[id];
+    document.querySelector(".detail-title")!.textContent = currentDetail.name;
+    (document.querySelector(".detail-link")! as HTMLLinkElement).href =
+      currentDetail.link;
+    document.querySelector(".detail-text")!.innerHTML = currentDetail.desc
+      .map(
+        (item) =>
+          `<div>${item
+            .map(
+              (item2) =>
+                `<div class="slide-in-line"><div class="slide-in-line-child">${item2}</div></div>`
+            )
+            .join("")}</div>`
+      )
+      .join("");
+  }
+  showInteractHint() {
+    document.querySelector(".interact-hint")?.classList.remove("hollow");
+  }
+  hideInteractHint() {
+    document.querySelector(".interact-hint")?.classList.add("hollow");
+  }
+  showReturnHint() {
+    document.querySelector(".return-hint")?.classList.remove("hollow");
+  }
+  hideReturnHint() {
+    document.querySelector(".return-hint")?.classList.add("hollow");
+  }
+  showEffectSwitch() {
+    document.querySelector(".effect-switch")?.classList.remove("hollow");
+  }
+  hideEffectSwitch() {
+    document.querySelector(".effect-switch")?.classList.add("hollow");
+  }
+  handleAbout() {
+    this.handleMenu();
+
+    document.querySelector(".about-link")?.addEventListener("click", () => {
+      this.openMenu();
     });
+
+    document
+      .querySelector(".about-return-link")
+      ?.addEventListener("click", () => {
+        this.closeMenu();
+      });
+  }
+  get isAtDetailPage() {
+    return !!this.currentActiveMesh;
+  }
+  handleMenu() {
+    const curveMask = document.querySelector("curve-mask") as any;
+    curveMask.addEventListener("open-end", () => {
+      document.querySelector(".about-page")?.classList.remove("hollow");
+    });
+  }
+  openMenu() {
+    if (!this.isAtDetailPage) {
+      this.disableSlider();
+    }
+
+    const curveMask = document.querySelector("curve-mask") as any;
+    const curveMaskMethods = curveMask?._instance?.exposed as any;
+    if (curveMaskMethods) {
+      curveMaskMethods.open();
+    }
+  }
+  closeMenu() {
+    if (!this.isAtDetailPage) {
+      this.enableSlider();
+    }
+
+    document.querySelector(".about-page")?.classList.add("hollow");
+
+    const curveMask = document.querySelector("curve-mask") as any;
+    const curveMaskMethods = curveMask?._instance?.exposed as any;
+    if (curveMaskMethods) {
+      curveMaskMethods.close();
+    }
   }
   slideInText(container = "") {
     gsap.fromTo(
